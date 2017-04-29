69070bdaa65ebb388454e32d4f7155ed33171fa0
pmstirpe <pmstirpe@users.noreply.github.com>
2017-04-29 14:40:49 -0400

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,18 @@
+/* Description	: Print Hello World on the screen
+
+   Author	: Peter Stirpe
+
+   Date		: 12/6/2016				*/
+
+
+#include<stdio.h>
+
+	int main()
+
+	{
+
+		printf(“Hello World\n”);
+
+		return 0;
+
+	}
